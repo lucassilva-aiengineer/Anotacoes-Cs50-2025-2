a53ec99d18883db9fda7e15ec29078d52d956e3e
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-04-23 17:32:25 -0300

    Iniciando o curso cs50 2025, 2

---
+++ conceitosIniciais/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+// Bibliotecas em c e funções que elas possuem. 
+
+int main(void){
+
+    printf("Hello, world! \n");
+
+
+    // Scape Sequence. 
+
+    // \n New line 
+    // \r 
+    // \'
+    // \\ 
+
+
+    return 0; 
+}
