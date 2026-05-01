31a3f4a345ad71c4dad12156424edb78d15930da
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-05-01 18:31:27 -0300

    Adicionando novas partes

---
+++ comandosCLI.c
@@ -0,0 +1,10 @@
+// cd navegamos até determinada pasta 
+// ls exibe uma lista com as pastas existentes no arquivo princípal 
+// mkdir cria uma pasta, diretório. 
+// mv pasta/arquivo.c pasta_2/
+// rm remove arquivo 
+// rmdir remove uma pasta 
+// mv hello.c conceitosInciais.c - Nós podemos utilizar os comandos mv para renomear arquivos 
+// cp arquivo1.c arquivo2.c - Nós podemos copiar o conteúdo do arquivo 1 para o arquivo 2.  
+// code cria um arquivo
+
