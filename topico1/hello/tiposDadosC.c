5535d4a4638f14210cc069352b7e9df59a834b8e
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-05-04 00:19:38 -0300

    Adicionando novas partes

--- topico1/hello/tiposDadosC.c
+++ topico1/hello/tiposDadosC.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <bool.h>
+#include <stdbool.h>
 
 
 void tipoDados(){
@@ -7,11 +7,37 @@ void tipoDados(){
     int numero; 
     numero = 10; 
 
-    printf("Inteiro: %d", numero)
+    printf("Inteiro: %d", numero); 
+
+    float numeroFloat = 10.5; 
+
+    printf("O número: %f", numeroFloat); 
+
+
+    double numeroDouble = 10.500005; 
+    printf("Double: %lf", numeroDouble); 
+
+
+    bool vendas = true; 
+    bool dividas = false; 
+
+    printf("Tivemos vendas: %d", vendas);
+
+
+
+    /* Específicadores de formato 
+
+        %c carcter
+        %f 
+        %d inteiro 
+        %li long int 
+        %s String  
+    */
 }
 
 int main(){
 
+    tipoDados();
 
     return 0; 
 }
